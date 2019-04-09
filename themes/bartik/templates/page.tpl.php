6d7c1149560e509ee2212c4c79a05eeddf27111c
Ethan Han <ethan.han@utoronto.ca>
2019-04-09 10:39:59 -0400

    fixed the bug

--- themes/bartik/templates/page.tpl.php
+++ themes/bartik/templates/page.tpl.php
@@ -85,6 +85,9 @@
  * @see html.tpl.php
  */
 ?>
+
+THIS IS FOR DEMO
+
 <div id="page-wrapper"><div id="page">
 
   <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
